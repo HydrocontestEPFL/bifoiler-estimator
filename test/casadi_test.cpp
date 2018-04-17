4e1784e7499ca870a915bd28553e3d28d55e1691
Michael Spieler <michael.spieler@epfl.ch>
2018-04-17 16:55:34 +0200

    More interesting example

--- test/casadi_test.cpp
+++ test/casadi_test.cpp
@@ -1,4 +1,5 @@
 #include <casadi/casadi.hpp>
+#include <math.h>
 #include <iostream>
 
 using namespace casadi;
@@ -7,4 +8,7 @@ int main()
 {
     SX x = SX::sym("x",1);
     std::cout << "x " << x << std::endl;
+    auto f = SX::vertcat({sin(x), pow(x, 2)});
+    std::cout << "f " << f << std::endl;
+    std::cout << "Jacobian(f) " << SX::jacobian(f,x) << std::endl;
 }
