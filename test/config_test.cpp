e84be3d7a0ce0128d81430e78c3afde2f8fa786c
Michael Spieler <michael.spieler@epfl.ch>
2018-04-17 15:04:25 +0200

    Testing YAML config files

---
+++ test/config_test.cpp
@@ -0,0 +1,17 @@
+#include "yaml-cpp/yaml.h"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2) {
+        std::cout << "usage: " << argv[0] << " path/to/file.yaml" << std::endl;
+        return -1;
+    }
+
+    // load test config
+    YAML::Node config = YAML::LoadFile(argv[1]);
+    std::cout << config << std::endl;
+
+    std::cout << "name " << config["name"].as<std::string>() << std::endl;
+    std::cout << "mass " << config["inertia"]["mass"].as<double>() << std::endl;
+}
