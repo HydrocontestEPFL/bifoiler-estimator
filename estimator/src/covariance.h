18f0176a15be235c20caa852267c37aba31054c5
Michael Spieler <michael.spieler@epfl.ch>
2018-06-13 19:42:42 +0200

    Use noise covariance matrices from original project

--- estimator/src/covariance.h
+++ estimator/src/covariance.h
@@ -4,51 +4,57 @@
 #define COVARIANCE_H
 
 #include <Eigen/Dense>
-#include <math.h>
+#include <cmath>
+
+#define DEG2RAD(x) ((M_PI / 180.0f) * (x))
 
 // Guesses of Process noise and Measurement Noise for the MTi-G-710 by XSENS
 
 template <typename Scalar>
 static Eigen::Matrix<Scalar, 18, 18> process_noise_covariance(void)
 {
+    const Scalar t_samp = 0.02; // TODO: parametrize
+
     Eigen::Matrix<Scalar, 18, 18> Q;
+    Eigen::Matrix<Scalar, 3, 1> Qv, Qw,Qr, Qa, Qbg, Qba;
+
+    Q.setZero();
 
     // Process Noise/Disturbances
     // ---> How far  can i be of in 0.02 seconds? (2 cm/0.02s = 1m/s)
 
     // Allthough the process noise appears in the RHS, it has the same unit as
     // the state, since Difference Equations!
-#if 0
+
     // Velocity Disturbance. unaccounted kinetics
-    Qv          = diag([0.01 0.8 0.4]);           // (m/s)^2, Wild guess
+    Qv << 0.01, 0.8, 0.4;             // [(m/s)^2], Wild guess
 
     // Angular Vel Disturbance. unaccounted kinetics
-    //Qw          = diag([0.025 0.025 0.1]);     // (rad/s)^2, Wild guess
-    Qw          = diag([0.1 0.8 1.2]);
+    //Qw << 0.025, 0.025, 0.1;          // (rad/s)^2, Wild guess
+    Qw << 0.1, 0.8, 1.2;              // [(rad/s)^2]
 
 
     // Position Disturbance. Emerge by the integration of kinetics that we don't model
-    Qr          = diag([0.05 0.05 0.03]);         // (m)^2, Wild guess
+    Qr << 0.05, 0.05, 0.03;           // [m^2], Wild guess
 
     // Attitude Disturbance
-    Qa          = diag([0.005 0.005 0.005]);          // (rad)^2, Wild guess
+    Qa << 0.005, 0.005, 0.005;        // [rad^2], Wild guess
 
     // Gyro bias stability/variance (Change of Bias with time, process noise of
     // noise state)
-    bs          = 10;                               // deg/h
-    sigma_b     = deg2rad(bs)/3600;                 // rad/s
-    Qbg         = sigma_b^2*eye(3);
+    Scalar bs_gyro = 10;                        // [deg/h]
+    Scalar sigma_b_gyro = DEG2RAD(bs_gyro)/3600;// [rad/s]
+    Qbg.setOnes();
+    Qbg *= pow(sigma_b_gyro, 2);
 
     // Accelerometer Bias stability/variance
-    bs          = 40;                               // mug
-    sigma_b     = bs/1e6*9.81*t_samp;               // m/s
-    Qba         = sigma_b^2*eye(3);                 // m^2/s^2
+    Scalar bs_acc = 40;                             // [mug]
+    Scalar sigma_b_acc = bs_acc/1e6*9.81*t_samp;    // [m/s]
+    Qba.setOnes();
+    Qba *= pow(sigma_b_acc, 2);     // [(m/s)^2]
 
-    // Process Covariance Matrix
-    Q           = blkdiag(Qv, Qw,Qr, Qa, Qbg, Qba);  // Process noise
-#endif
     Q.setZero();
-    Q.diagonal().setOnes(); // TODO: only for testing...
+    Q.diagonal() << Qv, Qw,Qr, Qa, Qbg, Qba;  // Process noise
 
     return Q;
 }
@@ -56,35 +62,34 @@ static Eigen::Matrix<Scalar, 18, 18> process_noise_covariance(void)
 template <typename Scalar>
 static Eigen::Matrix<Scalar, 12, 12> measurement_noise_covariance(void)
 {
+    const Scalar t_samp = 0.02; // TODO: parametrize
+
     // Measurement Noise
     Eigen::Matrix<Scalar, 12, 12> R;
+    Eigen::Matrix<Scalar, 3, 1> Rv, Rbg, Rr, Rba;
 
-#if 0
     // GNS Variance
-    Rr          = diag([2.5^2 2.5^2 5^2]);          // m^2
+    Rr << pow(2.5, 2), pow(2.5, 2), pow(5.0, 2); // [m^2]
 
-    Rv          = 0.05^2*eye(3);                    // (m/s)^2, std dev @ 30m/s
+    Rv.setOnes();
+    Rv *= pow(0.05, 2);                    // [(m/s)^2], std dev @ 30m/s
 
     // Gyro variance, noise (not density, since discrete time equation)
-    rho_sigma   = 0.01;                             // deg/s/sqrt(Hz)
-    sigma_gyro  = deg2rad(rho_sigma)*sqrt(1/t_samp);// rad/s
-    Rbg          = sigma_gyro^2*eye(3);
+    Scalar rho_sigma_gyro = 0.01; // [deg/s/sqrt(Hz)]
+    Scalar sigma_gyro = DEG2RAD(rho_sigma_gyro) * sqrt(1.0f/t_samp); // [rad/s]
+    Rbg.setOnes();
+    Rbg *= pow(sigma_gyro, 2);
 
     // Accelerometer Variance
     // Noise density. Max = 150, typical: 80
-    Scalar rho_sigma = 150;                              // mug/sqrt(Hz)
-    Scalar sigma_acc = rho_sigma / 1e6*9.81*t_samp*sqrt(10.f/t_samp);     // m/s
+    Scalar rho_sigma_acc = 150; // [mug/sqrt(Hz)]
+    Scalar sigma_acc = rho_sigma_acc / 1e6*9.81*t_samp*sqrt(1.0f/t_samp); // [m/s]
 
-    Eigen::Matrix<Scalar, 3, 1> Rba
     Rba.setOnes();
-    Rba *= sigma_acc^2*eye(3); // m^2/s^2
+    Rba *= pow(sigma_acc, 2); // [(m/s)^2]
 
     R.setZero();
     R.diagonal() << Rv, Rbg, Rr, Rba;
-#endif
-
-    R.setZero();
-    R.diagonal().setOnes(); // TODO: only for testing...
 
     return R;
 }
